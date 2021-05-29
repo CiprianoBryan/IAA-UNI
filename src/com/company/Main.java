de01dc6d05afbe8328468a67e0782779d47a112f
Bryan Cipriano <ciprianot.bryan@gmail.com>
2021-05-28 22:58:28 -0500

    Add .arff

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,19 +4,19 @@ import com.company.algorithms.*;
 
 public class Main {
     public static void main(String[] args) {
-        MLAlgoritmoJ48 mlJ48Test = new MLAlgoritmoJ48("file:///C:/Program%20Files/Weka-3-9-5/data/iris-test.arff");
-        MLAlgoritmoJ48 mlJ48Train = new MLAlgoritmoJ48("file:///C:/Program%20Files/Weka-3-9-5/data/iris-train.arff");
+        //MLAlgoritmoJ48 mlJ48Test = new MLAlgoritmoJ48("file:///C:/Bryan/UNI/21-1/IAA/Weka/src/com/datasets/iris/iris-test.arff");
+        //MLAlgoritmoJ48 mlJ48Train = new MLAlgoritmoJ48("file:///C:/Bryan/UNI/21-1/IAA/Weka/src/com/datasets/iris/iris-train.arff");
 
-        MLAlgorithmNaiveBayes mlBAYESTest = new MLAlgorithmNaiveBayes("file:///C:/Bryan/UNI/21-1/IAA/Weka/src/com/datasets/medical/medical-test.arff");
-        MLAlgorithmNaiveBayes mlBAYESTrain = new MLAlgorithmNaiveBayes("file:///C:/Bryan/UNI/21-1/IAA/Weka/src/com/datasets/medical/medical-train.arff");
+        //MLAlgorithmNaiveBayes mlBAYESTest = new MLAlgorithmNaiveBayes("file:///C:/Bryan/UNI/21-1/IAA/Weka/src/com/datasets/medical/medical-test.arff");
+        //MLAlgorithmNaiveBayes mlBAYESTrain = new MLAlgorithmNaiveBayes("file:///C:/Bryan/UNI/21-1/IAA/Weka/src/com/datasets/medical/medical-train.arff");
 
-        MLAlgoritmoSMOreg mlSMOTest = new MLAlgoritmoSMOreg("file:///C:/Bryan/UNI/21-1/IAA/Weka/src/com/datasets/medical/medical-test.arff");
-        MLAlgoritmoSMOreg mlSMOTrain = new MLAlgoritmoSMOreg("file:///C:/Bryan/UNI/21-1/IAA/Weka/src/com/datasets/medical/medical-test.arff");
+        //MLAlgoritmoSMOreg mlSMOTest = new MLAlgoritmoSMOreg("file:///C:/Bryan/UNI/21-1/IAA/Weka/src/com/datasets/medical/medical-test.arff");
+        //MLAlgoritmoSMOreg mlSMOTrain = new MLAlgoritmoSMOreg("file:///C:/Bryan/UNI/21-1/IAA/Weka/src/com/datasets/medical/medical-test.arff");
 
-        MLLibSVMTest mllSVMTest = new MLLibSVMTest("file:///C:/Program%20Files/Weka-3-9-5/data/iris-test.arff");
-        MLLibSVMTest mllSVMTrain = new MLLibSVMTest("file:///C:/Program%20Files/Weka-3-9-5/data/iris-train.arff");
+        //MLLibSVMTest mllSVMTest = new MLLibSVMTest("file:///C:/Bryan/UNI/21-1/IAA/Weka/src/com/datasets/iris/iris-test.arff");
+        //MLLibSVMTest mllSVMTrain = new MLLibSVMTest("file:///C:/Bryan/UNI/21-1/IAA/Weka/src/com/datasets/iris/iris-train.arff");
 
-        MLAlgoritmoReptree mlREPTREETest = new MLAlgoritmoReptree("file:///C:/Program%20Files/Weka-3-9-5/data/iris-test.arff");
-        MLAlgoritmoReptree mlREPTREETrain = new MLAlgoritmoReptree("file:///C:/Program%20Files/Weka-3-9-5/data/iris-test.arff");
+        //MLAlgoritmoReptree mlREPTREETest = new MLAlgoritmoReptree("file:///C:/Bryan/UNI/21-1/IAA/Weka/src/com/datasets/iris/iris-test.arff");
+        //MLAlgoritmoReptree mlREPTREETrain = new MLAlgoritmoReptree("file:///C:/Bryan/UNI/21-1/IAA/Weka/src/com/datasets/iris/iris-train.arff");
     }
 }
